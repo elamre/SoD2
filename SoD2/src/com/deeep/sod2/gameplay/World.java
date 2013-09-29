9c978a5ce442996364e2cc7f8e6e611c352feafe
Elmar <Eva.Electronics@outlook.com>
2013-09-29 20:37:49 +0200

    o

---
+++ SoD2/src/com/deeep/sod2/gameplay/World.java
@@ -0,0 +1,18 @@
+package com.deeep.sod2.gameplay;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Elmar
+ * Date: 9/29/13
+ * Time: 7:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class World {
+    private ArrayList<Object> objects = new ArrayList<Object>();
+
+    public World() {
+
+    }
+}
