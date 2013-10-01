d1006968cc9ec6f98e1feb7e2a7e21ace3ed646a
Kruhlmann <andreaskruhlmann@gmail.com>
2013-10-01 16:04:08 +0200

    Particle works

--- SoD2/src/com/deeep/sod2/graphics/Particle.java
+++ SoD2/src/com/deeep/sod2/graphics/Particle.java
@@ -62,7 +62,7 @@ public class Particle {
     public void draw(SpriteBatch graphics){
         if(lifespan<0 && lifespan!=-1) return;
         double t = lifespan;
-        if(lifespan!=-1) t=255;
+        if(lifespan==-1) t=255;
         ShapeRenderer.setColor(new Color(r, g, b, (float)t));
         ShapeRenderer.drawRectangle(graphics, location.x, location.y, width, height, true);
     }
